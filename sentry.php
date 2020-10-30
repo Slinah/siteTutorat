a6e04826e258650bacaf32c8d7a902e1f89650b4
Slinah <45566113+Slinah@users.noreply.github.com>
2020-10-30 11:28:25 +0100

    Sentry fonctionne

--- sentry.php
+++ sentry.php
@@ -1,8 +1,6 @@
 <?php
+require_once __DIR__ . '/requests/insert.php';
+
 require(__DIR__ . '/vendor/autoload.php');
-// require_once __DIR__ . '/vendor/sentry/sentry/src/SentrySdk.php';
-// use function Sentry\init;
 
-init(['dsn' => 'https://f6abc5be082f4c52ac8a34b788e4b618@o469219.ingest.sentry.io/5498360' ]);
-
-throw new Exception("My first Sentry error!");
+Sentry\init(['dsn' => 'https://f6abc5be082f4c52ac8a34b788e4b618@o469219.ingest.sentry.io/5498360' ]);
