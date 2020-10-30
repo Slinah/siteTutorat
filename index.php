6c404d36e518d179d8eabc60c4bc159c7a90ef57
Slinah <45566113+Slinah@users.noreply.github.com>
2020-10-30 11:29:38 +0100

    Sentry (#6)
    * That's not cool sentry
    
    * Sentry fonctionne

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+require_once __DIR__ . '/sentry.php';
+try {
+    header("location: connexion/co.php?co=newco");
+} catch (Exception $e) {
+    throw new Exception('Redirect from / : ' . $e);
+}
 
-header("location: connexion/co.php?co=newco");
 ?>
---
+++ sentry.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/requests/insert.php';
+
+require(__DIR__ . '/vendor/autoload.php');
+
+Sentry\init(['dsn' => 'https://f6abc5be082f4c52ac8a34b788e4b618@o469219.ingest.sentry.io/5498360' ]);
