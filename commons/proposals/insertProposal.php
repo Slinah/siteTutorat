32b66f8daaf0a86cc3a39be0cc64ec05bbd05278
Slinah <cedr.men@gmail.com>
2020-10-31 12:58:46 +0100

    Proposals

--- commons/proposals/insertProposal.php
+++ commons/proposals/insertProposal.php
@@ -9,17 +9,19 @@ require_once '../../requests/UUID.php';
 $userTkn = selectTokenByIdPersonne($_SESSION['id_personne']);
 if (isset($userTkn) && $userTkn != null) {
 
-    if (isset($_POST['matiere'])) {
-        $idMatiere = $_POST['matiere'];
-    } else {
-        header("location: proposeCourse.php?proposal=error");
-    }
+    $idMatiere = isset($_POST['matiere']) ? $_POST['matiere'] : null;
+    $niveau = isset($_POST['classe']) ? $_POST['classe'] : null;
+    // if (isset($_POST['matiere'])) {
+    //     $idMatiere = $_POST['matiere'];
+    // } else {
+    //     header("location: proposeCourse.php?proposal=error");
+    // }
 
-    if (isset($_POST['classe'])) {
-        $niveau = $_POST['classe'];
-    } else {
-        header("location: proposeCourse.php?proposal=error");
-    }
+    // if (isset($_POST['classe'])) {
+    //     $niveau = $_POST['classe'];
+    // } else {
+    //     header("location: proposeCourse.php?proposal=error");
+    // }
 
     $tkn = substr(base_convert(sha1(uniqid(mt_rand())), 16, 36), 0, 32);
     function secuStg($length = 15)
@@ -35,19 +37,19 @@ if (isset($userTkn) && $userTkn != null) {
 
     $idProposition = strtoupper(UUID::v4());
 
-    if (verifExistPropositionByIdMatiereIdPromo($idMatiere, $niveau) == 'aucune proposition avec ces id') {
-        insertProposition($idProposition, $idMatiere, secuStg());
-        insertPropositionPromo($idProposition, $niveau);
-        insertPersonneProposition($_SESSION['id_personne'], $idProposition);
-        insertLogProposition($idProposition, date("Y-m-d H:i:s", $localDate));
-        deletePropositionWithNullMatiere();
-        updateToken($tkn, $_SESSION['id_personne']);
-        header("location: proposeCourse.php?proposal=success");
-    } else {
-        insertPersonneProposition($_SESSION['id_personne'], selectPropositionMatierePromo($idMatiere, $niveau));
-        updateToken($tkn, $_SESSION['id_personne']);
-        header("location: proposeCourse.php?proposal=creaupvoted");
-    }
+    // if (verifExistPropositionByIdMatiereIdPromo($idMatiere, $niveau) == 'aucune proposition avec ces id') {
+    //     insertProposition($idProposition, $idMatiere, secuStg());
+    //     insertPropositionPromo($idProposition, $niveau);
+    //     insertPersonneProposition($_SESSION['id_personne'], $idProposition);
+    //     insertLogProposition($idProposition, date("Y-m-d H:i:s", $localDate));
+    //     deletePropositionWithNullMatiere();
+    //     updateToken($tkn, $_SESSION['id_personne']);
+    //     header("location: proposeCourse.php?proposal=success");
+    // } else {
+    //     insertPersonneProposition($_SESSION['id_personne'], selectPropositionMatierePromo($idMatiere, $niveau));
+    //     updateToken($tkn, $_SESSION['id_personne']);
+    //     header("location: proposeCourse.php?proposal=creaupvoted");
+    // }
 } else {
     echo 'Bien essayé';
 }
