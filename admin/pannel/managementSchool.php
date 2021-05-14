8c8268e17f248cbc443a9a07a90c852c5a3a2322
Slinah <cedr.men@gmail.com>
2021-05-15 01:25:07 +0200

    C'est de la merde

--- admin/pannel/managementSchool.php
+++ admin/pannel/managementSchool.php
@@ -22,14 +22,14 @@ if (!isset($_SESSION["role"]) || $_SESSION["role"] != 1) {
     <?php include_once '../../bases/menu.php'; ?>
     <div class="container">
         <ul data-role="treeview">
-            <?php foreach (selectEcoles() as $e) { ?>
-                <li data-icon="<span class='mif-library'></span>" data-caption="<?php echo $e['intitule'] ?>">
+            <?php foreach (selectEcoles() as $ke=>$e) { ?>
+                <li id="adminEcole<?php echo $ke ?>" data-caption="<?php echo $e['intitule'] ?>" data-collapsed="true">
                     <ul>
-                        <?php foreach (selectPromosByIdEcole($e['id_ecole']) as $p) { ?>
-                            <li data-icon="<span class='mif-library'></span>" data-caption="<?php echo $p['intitule'] ?>">
+                        <?php foreach (selectPromosByIdEcole($e['id_ecole']) as $kp=>$p) { ?>
+                            <li id="adminPromo<?php echo $kp ?>" data-caption="<?php echo $p['intitule'] ?>" data-collapsed="true">
                                 <ul>
-                                    <?php foreach (selectClassesPromoEcoles($p['intitule'], $e['intitule']) as $c) { ?>
-                                        <li data-icon="<span class='mif-library'></span>" data-caption="<?php echo $c['classe'] ?>"></li>
+                                    <?php foreach (selectClassesPromoEcoles($p['intitule'], $e['intitule']) as $kc=>$c) { ?>
+                                        <li id="adminClasse<?php echo $kc ?>" data-caption="<?php echo $c['classe'] ?>" data-collapsed="true"></li>
                                     <?php } ?>
                                 </ul>
                             </li>
@@ -41,5 +41,20 @@ if (!isset($_SESSION["role"]) || $_SESSION["role"] != 1) {
     </div>
 </body>
 <script src="../../js/activeMenu.js"></script>
+<script>
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+    for (let i = 0; i < 100; i++) {
+        $("#adminEcole"+i).css("color", getRandomColor());    
+        $("#adminPromo"+i).css("color", getRandomColor());
+        $("#adminClasse"+i).css("color", getRandomColor());
+    }
+</script>
 
 </html>
