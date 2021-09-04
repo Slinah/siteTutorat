8ef2074882030d468e0882879e4dbe25cd3a3bef
Slinah <45566113+Slinah@users.noreply.github.com>
2021-09-04 13:14:02 +0200

    Update conf.php

--- conf/conf.php
+++ conf/conf.php
@@ -3,7 +3,7 @@
 class Config {
     const SERVERNAME="localhost";
     //const DBNAME="graph";
-    const DBNAME="tutoratrefonte";
+    const DBNAME="tutorat";
     const USER="admin";
     const PASSWORD="tutorat2020!";
 }
